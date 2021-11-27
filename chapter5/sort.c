5a5e9876fc289b2bcd41db9813e910ab5bec442c
carlschader <carlschader@gmail.com>
2021-11-26 21:18:10 -0700

    5-15 in progress

--- chapter5/sort.c
+++ chapter5/sort.c
@@ -7,6 +7,15 @@
 
 char *lineptr[MAXLINES];
 
+void lower(char *s) {
+    int diff = 'A' - 'a';
+    for (; *s != '\0'; ++s) {
+        if (*s >= 'A' && *s <= 'Z') {
+            *s = diff < 0 ? *s - diff : *s + diff;
+        }
+    }
+}
+
 int stringcomp(const void *a, const void* b) {
     return strcmp(*(const char **)a, *(const char **)b);
 }
@@ -16,7 +25,22 @@ int reversestringcomp(const void *a, const void* b) {
 }
 
 int foldcase(const void *a, const void *b) {
-    return 0;
+    const char *x = *(const char **)a;
+    const char *y = *(const char **)b;
+    for (; *x != '\0'; ++x, ++y) {
+        
+    }
+
+    return strcmp(x, y);
+}
+
+int reversefoldcase(const void *a, const void *b) {
+    char *x = *(char **)a;
+    char *y = *(char **)b;
+    lower(x);
+    lower(y);
+
+    return strcmp(y, x);
 }
 
 int numericcomp(const void *a, const void *b) {
@@ -28,14 +52,16 @@ int reversenumericcomp(const void *a, const void *b) {
 }
 
 int main(int argc, char *argv[]) {
-    int numeric, reverse;
-    numeric = reverse = 0;
+    int numeric, reverse, fold;
+    numeric = reverse = fold = 0;
 
     for (++argv ;argc > 1; --argc, ++argv) {
         if (strcmp(*argv, "-n") == 0)
             numeric = 1;
         if (strcmp(*argv, "-r") == 0)
             reverse = 1;
+        if (strcmp(*argv, "-f") == 0)
+            fold = 1;
     }
 
     int nlines = readlines(lineptr, MAXLINES);
@@ -45,6 +71,12 @@ int main(int argc, char *argv[]) {
             qsort(lineptr, nlines, sizeof(char *), reversenumericcomp);
         } else {
             qsort(lineptr, nlines, sizeof(char *), numericcomp);
+        }
+    } else if (fold) {
+        if (reverse) {
+            qsort(lineptr, nlines, sizeof(char *), reversefoldcase);
+        } else {
+            qsort(lineptr, nlines, sizeof(char *), foldcase);
         }
     } else {
         if (reverse) {
